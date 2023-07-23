67f7bae327838925956816dc47d00dd47d36c488
vasilii <batuyev_1991@mail.ru>
2023-07-23 19:16:17 +0300

    1

--- conv_web/main/models.py
+++ conv_web/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class Converter(models.Model):
+#     summa = models.FloatField('Сумма', max_length=50),
+#     conv = models.FloatField('Курс', max_length=50),
+
+#     def __str__(self):
+#         return self.summa
--- conv_web/main/views.py
+++ conv_web/main/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
+import requests
+from urllib.parse import urljoin
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+
+        spisok = ['RUB', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD',
+        'CAD', 'CDF', 'CHF', 'CLP', 'CNY', 'COP', 'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'FOK', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK',
+        'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE', 'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TVD', 'TWD', 'TZS', 'UAH',
+        'UGX', 'USD', 'UYU', 'UZS', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWL']
+
+        chto_menyaem = request.POST.get('from-curr')
+        na_chto_menyaem = request.POST.get('from-to')
+        summa = request.POST.get('summa')
+        otvet = ''
+
+        if  chto_menyaem is not None:
+            obmen = urljoin("https://v6.exchangerate-api.com/v6/681ae99e01565568f5986cbb/latest/", chto_menyaem)
+            response = requests.get(url=obmen).json()
+            kurs = response['conversion_rates'][na_chto_menyaem]
+            otvet = float(summa)*float(kurs)
+        else:
+           pass
+
+        context = {
+                'summa': summa,
+                # 'response': response,
+                'spisok': spisok,
+                'otvet': otvet,
+                'na_chto_menyaem': na_chto_menyaem,
+                'chto_menyaem': chto_menyaem,
+                }
+        return render(request, 'main/pattern.html', context)
